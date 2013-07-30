c8e0d27ffe3b44e2ee422fc592df3a8789aa67e9
zhangyoumin <zhangyoumin@localhost.(none)>
2013-07-30 14:26:08 +0800

    modify log

--- src/server.go
+++ src/server.go
@@ -1,14 +1,14 @@
 package main
 
 import (
-  "fmt"
   "net/http"
   "route"
   "util"
+  "log"
 )
 
 func main() {
   route.Init()
-  fmt.Printf("Port: %s listening...\n", util.Datacfg().Section.Port)
+  log.Printf("Port: %s listening...\n", util.Datacfg().Section.Port)
   http.ListenAndServe(":" + util.Datacfg().Section.Port, nil)
 }
